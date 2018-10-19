d1fef411737fc6e8d8a226c98e374eb57e02ac60
hiscaler <hiscaler@gmail.com>
2018-10-20 00:06:07 +0800

    路由 - 2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,27 +8,19 @@ import { HeroService } from "../hero.service";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
   heroes: Hero[];
-
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) {
 
+  }
+
+
+  ngOnInit() {
+    this.getHeroes()
   }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
-  ngOnInit() {
-    this.getHeroes()
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 }
